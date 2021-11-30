418db213ae15f017b78fadd934c763adb236a23f
Breanna Dodd <bdodd@salesforce.com>
2021-11-30 13:04:12 -0700

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+class AddInitialRaces < ActiveRecord::Migration[5.0]
+  def up
+    race = Race.create(name: "24 hours Nürburgring", date: "2003-11-12",
+                finish_place: 1, time: 12)
+    race.injuries.create(body_part: "arm", injury_date: race.date,
+                      broken: true,
+                      severity: 1,
+                      recovery_date: "2003-11-13",)
+
+  end
+end
+
