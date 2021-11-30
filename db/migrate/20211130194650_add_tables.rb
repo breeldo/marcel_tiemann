32486674ede5b4db282674dd780885b94d6a489a
Breanna Dodd <bdodd@salesforce.com>
2021-11-30 12:51:07 -0700

    first tables

---
+++ db/migrate/20211130194650_add_tables.rb
@@ -0,0 +1,20 @@
+class AddTables < ActiveRecord::Migration[6.1]
+  def change
+    create_table :injuries do |t|
+      t.string :body_part
+      t.date   :injury_date
+      t.bool   :broken
+      t.integer :severity
+      t.date    :recovery_date
+      t.references :races
+    end
+
+    create_table :races do |t|
+      t.date :date
+      t.string :name
+      t.integer :time
+      t.integer :finish_place
+    end
+
+  end
+end
